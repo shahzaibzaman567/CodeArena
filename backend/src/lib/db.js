7f8f9fdab11b58638ea1d4d9779dd1c8890fe322
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2026-02-25 09:59:38 +0500

    add condition in db file

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,6 +4,9 @@ import {ENV} from "./env.js"
 
 export const connectDB= async () => {
 try{
+    if(!ENV.DB_URL){
+          throw new Error ("DB_URL is not define in environment variable")
+    }
     const connect=await mongoose.connect(ENV.DB_URL)
     console.log("✅Connected to mongoDB : " , connect.connection.host )
 }catch(err){
