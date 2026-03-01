b0438ded99d1d5e3a1f351c08f6901e2ecb93557
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2026-03-01 10:41:32 +0500

    solve deplyment problems

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,43 +1,39 @@
 import { connectDB } from "./db.js";
-import  { Inngest } from "inngest";
+import { Inngest } from "inngest";
 import User from "../models/User.js";
 
-export const inngest = new Inngest({id:"code-arena"})
+export const inngest = new Inngest({ id: "code-arena" });
 
-const syncUser=inngest.createFunction(
-    {id:"sync-user"},
-    {event:"clerk/user.created"},
-    async({event})=>{
-        await connectDB()
-            const {id,email_addresses,first_name,last_name,image_url}=event.data
-            const newUser={
-                clerkId:id,
-                email:email_addresses[0].email_address,
-                name:`${    first_name || ""} ${last_name || ""}`,
-                profileImage:image_url
-            }
-         await   User.create(newUser)
-    }
-)
-//delet user from database
-const DeleteUserFromDB=inngest.createFunction(
-    {id:"delete-user-from-db"},
-    {event:"clerk/user.delted"},
-    async({event})=>{
-        await connectDB()
-            const {id}=event.data
-           await User.deleteOne({clerkId:id})
-    }
-)
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
 
-export const Functions = [syncUser,DeleteUserFromDB];
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
+    const newUser = {
+      clerkId: id,
+      email: email_addresses?.[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`,
+      profileImage: image_url,
+    };
 
+    await User.create(newUser);
+  }
+);
 
+// delete user
+const deleteUserFromDB = inngest.createFunction(
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
 
+    const { id } = event.data;
 
+    await User.deleteOne({ clerkId: id });
+  }
+);
 
-
-
-
-
+export const functions = [syncUser, deleteUserFromDB];
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,16 @@ import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import cors from "cors"
-import server from "inngest/express";
-import { Inngest } from "inngest";
-import { Functions, inngest } from "./lib/inngest.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
 dotenv.config();
 
 const app = express();
 const __dirname = path.resolve()
 //middleware
 app.use(express.json())
-app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))//credentials: true meaning =>server allows a browser to on req
-app.use("/api/inngest",server({client:inngest,Functions}))
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))//credentials: true meaning =>serve allows a browser to on req
+app.use("/api/inngest",serve({client:inngest,functions}))
 
 
 //get health api
@@ -26,18 +25,25 @@ app.get("/books", (req, res) => {
   res.status(200).json({ message: "this is the book end point" });
 });
 
-const serverStart = async () =>{
-  
-  try{
-    app.listen(ENV.port,() => {  console.log(`server started on port ${ENV.port}`)});
-    connectDB()
-  }catch(err){
-    console.log(err)
-  }
-  
+async function ConnectDB(){
+ try{
+       connectDB()
+ }catch(err){
+console.log(err)
+ }
 }
-
-serverStart()
+ConnectDB()
+// const serverStart = async () =>{
+  
+//   try{
+  //   }catch(err){
+    //     console.log(err)
+    //   }
+    
+    // }
+    
+        app.listen(ENV.port,() => {  console.log(`server started on port ${ENV.port}`)});
+// serverStart()
 //make our app ready for deployment
 // if(ENV.NODE_ENV === "production"){
 //     app.use(express.static(path.join(__dirname,"frontend","dist")))
