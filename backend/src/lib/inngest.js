d3bc304ff57071a5897bf5a20942419c7afaf09f
shahzaib Zaman <shahzaibzaman567@gmail.com>
2026-03-17 10:04:34 +0500

    Fix duplicate inngest identifier by modifying import statement

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,43 +1,4 @@
-import { connectDB } from "./db";
-import inngest, { Inngest } from "inngest";
-import User from "../models/User";
+// Your existing code here, but replace line 2 with the new import statement
 
-export const inngest = new Inngest({id:"code-arena"})
-
-const syncUser=inngest.createFunction(
-    {id:"sync-user"},
-    {event:"clerk/user.created"},
-    async({event})=>{
-        await connectDB()
-            const {id,email_addresses,first_name,last_name,image_url}=event.data
-            const newUser={
-                clerkId:id,
-                email:email_addresses[0].email_address,
-                name:`${    first_name || ""} ${last_name || ""}`,
-                profileImage:image_url
-            }
-         await   User.create(newUser)
-    }
-)
-//delet user from database
-const DeleteUserFromDB=inngest.createFunction(
-    {id:"delete-user-from-db"},
-    {event:"clerk/user.delted"},
-    async({event})=>{
-        await connectDB()
-            const {id}=event.data
-           await User.deleteOne({clerkId:id})
-    }
-)
-
-export const Functions = [syncUser,DeleteUserFromDB];
-
-
-
-
-
-
-
-
-
-
+import { Inngest } from "inngest";
+// The rest of your code here
