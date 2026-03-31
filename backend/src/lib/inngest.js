2794d4b95f0281765026561c83b2c27109fdf104
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2026-03-31 09:20:38 +0500

    correct event name

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,13 +1,15 @@
 import { Inngest } from "inngest";
-// Note: Is file mein connectDB aur User model tabhi import karein jab zaroorat ho
 import User from "../models/User.js"; 
+import { connectDB } from "./db.js";
 
 export const inngest = new Inngest({ id: "code-arena" });
 
+// 1. Function: User Create ya Update ke liye
 export const syncUser = inngest.createFunction(
   { id: "sync-user" },
-  { event: "clerk/user.created" }, // Clerk event structure check karein
-  async ({ event, step }) => {
+  { event: "user.created" }, // Clerk ka sahi event name
+  async ({ event }) => {
+    await connectDB();
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
     const userPayload = {
@@ -17,11 +19,23 @@ export const syncUser = inngest.createFunction(
       profileImage: image_url,
     };
 
-    // Database operation
     await User.findOneAndUpdate({ clerkId: id }, userPayload, { upsert: true });
-    return { message: "User synced" };
+    return { message: "User created/synced" };
   }
 );
 
-// Hamesha 'functions' (small f) export karein
-export const functions = [syncUser]; 
+// 2. Function: User Delete karne ke liye
+export const deleteUser = inngest.createFunction(
+  { id: "delete-user" },
+  { event: "user.deleted" }, // Clerk jab user delete karega
+  async ({ event }) => {
+    await connectDB();
+    const { id } = event.data; // Clerk delete event mein sirf ID bhejta hai
+
+    await User.findOneAndDelete({ clerkId: id });
+    return { message: "User deleted from MongoDB" };
+  }
+);
+
+// Dono functions ko export karein small 'f' ke saath
+export const functions = [syncUser, deleteUser];
