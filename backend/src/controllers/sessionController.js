e493839db79250f6fe2771f01a8721d4a5b234b7
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2026-04-21 12:44:35 +0500

    refactoring controller.js

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -96,10 +96,19 @@ export async function joinSession(req, res) {
         const clerkId = req.user.clerkId;
         const session = await Session.findById(id);
         if (!session) return res.status(404).json({ message: "Session not found" });
+        
         // check if session is already full - has a participant
-        if (session.participant)
-            return res.status(404).json({ message: "Session is full" });
+        
+        if (session.participant) return res.status(409).json({ message: "Session is full" });
+        
+        if(session.status !== "active"){
+            return res.status(400).json({message:"Cannot join a completed session"})
+        }
+if(session.host.toString() === userId.toString()){
+    return res.status(400).json({message:"Host cannot join their on session participant"})
+}
         session.participant = userId;
+        
         await session.save();
 
         const channel = chatClient.channel("messaging", session.callId);
@@ -121,17 +130,17 @@ export async function endSession(req, res) {
         if (session.host.toString !== userId.toString()) {
             return res.json({ message: "On;ly the can end the session" }).status(403);
         }
+        // delete stream video call 
+        const call = streamClient.video.call("default", session.clerkId)
+        await call.delete({ hard: true })
+        
+        //delete stream chat channel 
+        const channel = chatClient.channel("messaging", session.callId)
+        await channel.delete()
+        
         //check if session is already completed
         session.status = "completed";
         await session.save;
-// delete stream video call 
-const call =streamClient.video.call("default",session.clerkId)
-await call.delete({hard:true})
-
-//delete stream chat channel 
-const channel =chatClient.channel("messaging",session.callId)
-await channel.delete()
-
 
 
         res.status(200).json({ session, message: "Message ended successfully" });
